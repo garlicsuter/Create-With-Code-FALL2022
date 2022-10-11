8410acf7e8623f2d36fd96a86fa01af43642deb7
Garlicsuter <markasuter@gmail.com>
2022-10-11 11:13:40 -0400

    jumpy man updates

--- Prototype 3 - Jumpy Man/Assets/Course Library/Scripts/SpawnManager.cs
+++ Prototype 3 - Jumpy Man/Assets/Course Library/Scripts/SpawnManager.cs
@@ -9,15 +9,21 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, .5f, 0);
     public float startDelay = 1;
     public float repeatRate = 1;
+    private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, SpawnManagerObj.transform.position, obstaclePrefab.transform.rotation);
+        if(playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, SpawnManagerObj.transform.position, Quaternion.identity);
+        }
+        
     }
 }
